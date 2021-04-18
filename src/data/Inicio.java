25f48de34fb86090dac9cd3893dbe3166b932e87
Juan A. Barrera R <jbarrera0314@gmail.com>
2021-04-18 16:02:33 -0500

    Clases Pila-Inicio creadas

---
+++ src/data/Inicio.java
@@ -0,0 +1,20 @@
+package data;
+import java.util.*;
+
+public class Inicio {
+    public static void main(String args[]) {
+
+        while (true) {
+            System.out.println("Presione 1 para añadir un animal");
+            System.out.println("Presione 2 para añadir un animal");
+            Scanner num1 = new Scanner(System.in);
+            int opcion = num1.nextInt();
+
+
+            switch (opcion) {
+
+            }
+            break;
+        }
+    }
+}
