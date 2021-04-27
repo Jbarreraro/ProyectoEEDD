a0853e804fd59fb3c58bdb58fec1e720940d86a6
Juan A. Barrera R <jbarrera0314@gmail.com>
2021-04-26 21:31:17 -0500

    Historias clinicas organizadas

---
+++ src/datos/Nodo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+/**
+ *
+ * @author sanch
+ */
+public class Nodo<T>{
+    private Nodo siguiente;
+    private T dato;
+
+    public Nodo(T dato){
+        this.dato = dato;
+
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+
+
+}
